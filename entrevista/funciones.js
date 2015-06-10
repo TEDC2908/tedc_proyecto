56281ee3afae9e5fd0fbf1dcf84a230aad573340
Yaxche Almazan Pardo <yaxche2@gmail.com>
2015-06-09 23:30:51 -0500

    Actualización
    Se establece una variable para contar los errores
    Se establece una variable para llevar la cuenta de las palabras
    contestadas

--- entrevista/funciones.js
+++ entrevista/funciones.js
@@ -1,8 +1,12 @@
 var numAleatorio = Math.floor(Math.random() * (5)) + 1;
 var longPalabra=0;
+var contadorPalabras = 0;
+var equivocaciones=0;
+
 $(document).ready(principal(numAleatorio));
 
 function principal(numeroAle){
+	console.log("Se han cometido: "+equivocaciones);
 $.ajax({ // Con la funcion ajax cargamos el XML
 	url: 'entrevista.xml',
 	dataType: 'xml',
@@ -19,12 +23,11 @@ $.ajax({ // Con la funcion ajax cargamos el XML
 				longPalabra = $(this).attr('palabra').length;
 				var palabraSeparada = $(this).attr('palabraSeparada').split(",");
 				var ayuda = $(this).attr('ayuda');
-				console.log(palabraSeparada);
+				//console.log(palabraSeparada);
 				$("#frase").append("<p>"+$(this).attr('frase')+"</p>");
 				//Creamos los input text deacuerdo al numero de letras de la palabra
 				for(i = 0;i < longPalabra; i++){
 					$("#letras-palabra").append('<input id='+i+' type=text size=1>');
-
 				}
 				//$(':input:enabled:visible:first').focus();
 				$("#mostrarAyuda").click(function(){
@@ -35,14 +38,14 @@ $.ajax({ // Con la funcion ajax cargamos el XML
 				var textFocus;
 				inputLetra.focus(function (e){
 				textFocus = $(this).attr('id');
-				console.log($(this).attr('id'));
+				//console.log($(this).attr('id'));
 				});
 				var errores=2; //Establecemos los intentos permitidos
 				$("#intentos").html("<p>Intentos: "+errores+"</p>");
 				
 				//Disparamos un evento si se teclea algo dentro de los input text
 				inputLetra.keypress(function (e){
-					console.log($(this).attr('id'));
+					//console.log($(this).attr('id'));
 					//Con el sigueinte if validamos la letra y el input text donde se escribio
 							if(String.fromCharCode(e.which) == palabraSeparada[$(this).attr("id")] && textFocus == $(this).attr("id")){
 							//Si es correcta la letra hacemos lo siguiente:
@@ -57,6 +60,9 @@ $.ajax({ // Con la funcion ajax cargamos el XML
 								$("#frase").html('');
 								$("#letras-palabra").html('');
 								principal(numAleatorio2);
+								contadorPalabras +=1;
+								console.log("La cuenta va en:"+contadorPalabras);
+
 							}
 							errores=2;
 							$("#intentos").html("<p>Intentos: "+errores+"</p>");
@@ -72,7 +78,19 @@ $.ajax({ // Con la funcion ajax cargamos el XML
 								$(this).val('');
 								$(this).prop('disabled',true);
 								$(this).val(palabraSeparada[$(this).attr("id")]); //Ponemos la letra correcta
-								$(this).next().focus();//Nos vemos al siguiente input text
+								equivocaciones +=1;
+								if($(this).attr('id') != longPalabra-1){
+								$(this).next().focus();
+							}else{
+								alert("Palabra Completa");
+								var numAleatorio2 = Math.floor(Math.random() * (5)) + 1;
+								$("#frase").html('');
+								$("#letras-palabra").html('');
+								principal(numAleatorio2);
+								contadorPalabras +=1;
+								console.log("La cuenta va en: "+contadorPalabras);
+
+							}
 								errores = 2;//Restablecemos los intentos
 								$("#intentos").html("<p>Intentos: "+errores+"</p>");
 							}
@@ -81,6 +99,15 @@ $.ajax({ // Con la funcion ajax cargamos el XML
 
 										
 				});
+				if(contadorPalabras ==2){
+					$("#frase").hide();
+					$("#letras-palabra").hide();
+					$("#mensajesSistema").hide();
+					$("#intentos").hide();
+					$("#mostrarAyuda").hide();
+					$("#resultados").append("<p>Cometitaste "+equivocaciones+" equivocaciones</p>");
+					$("#resultados").append("<p>Tu calificacion es: </p>");
+				}
 
 			}
 		});
@@ -89,4 +116,6 @@ $.ajax({ // Con la funcion ajax cargamos el XML
 		$("#letras-palabra").text("Error al obtener xml");
 	}
 });
+
+
 }
