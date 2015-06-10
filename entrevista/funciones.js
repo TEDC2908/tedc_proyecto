46ff6d6942dcbc5e68087a582101275102cec723
Yaxche Almazan Pardo <yaxche2@gmail.com>
2015-06-09 19:00:03 -0500

    Se agregan mejoras
    Se agregan mas palabras al XML
    Al iniciar se elige una palabra aleatoriamente
    Al terminar de escribir la palabra aparece un mensaje Palabra Completa
    Ahora al terminar de escribir la palabra se carga una nueva
    aleatoriamente del XML
    Se agrega el botón de ayuda que despliega una pista sobre la palabra
    faltante

--- entrevista/funciones.js
+++ entrevista/funciones.js
@@ -1,3 +1,8 @@
+var numAleatorio = Math.floor(Math.random() * (5)) + 1;
+var longPalabra=0;
+$(document).ready(principal(numAleatorio));
+
+function principal(numeroAle){
 $.ajax({ // Con la funcion ajax cargamos el XML
 	url: 'entrevista.xml',
 	dataType: 'xml',
@@ -8,9 +13,12 @@ $.ajax({ // Con la funcion ajax cargamos el XML
 			//Obtenemos el nivel
 			var pregunta = $(this).attr('nivel');
 			//Como ejemplo utilizamos el nivel 1-1, luego se hara aleatoriamente
-			if(pregunta == "11"){
-				var longPalabra = $(this).attr('palabra').length;
+			
+			console.log(numeroAle);
+			if(pregunta == "1"+numeroAle){
+				longPalabra = $(this).attr('palabra').length;
 				var palabraSeparada = $(this).attr('palabraSeparada').split(",");
+				var ayuda = $(this).attr('ayuda');
 				console.log(palabraSeparada);
 				$("#frase").append("<p>"+$(this).attr('frase')+"</p>");
 				//Creamos los input text deacuerdo al numero de letras de la palabra
@@ -19,7 +27,9 @@ $.ajax({ // Con la funcion ajax cargamos el XML
 
 				}
 				//$(':input:enabled:visible:first').focus();
-	
+				$("#mostrarAyuda").click(function(){
+					alert(ayuda);
+				});
 
 				var inputLetra = $("input");
 				var textFocus;
@@ -34,16 +44,24 @@ $.ajax({ // Con la funcion ajax cargamos el XML
 				inputLetra.keypress(function (e){
 					console.log($(this).attr('id'));
 					//Con el sigueinte if validamos la letra y el input text donde se escribio
-						if(String.fromCharCode(e.which) == palabraSeparada[$(this).attr("id")] && textFocus == $(this).attr("id")){
+							if(String.fromCharCode(e.which) == palabraSeparada[$(this).attr("id")] && textFocus == $(this).attr("id")){
 							//Si es correcta la letra hacemos lo siguiente:
 							$("#mensajesSistema").html("<p>Correcto</p>")
 							$(this).val(String.fromCharCode(e.which));
 							$(this).prop('disabled',true);
-							$(this).next().focus();
+							if($(this).attr('id') != longPalabra-1){
+								$(this).next().focus();
+							}else{
+								alert("Palabra Completa");
+								var numAleatorio2 = Math.floor(Math.random() * (5)) + 1;
+								$("#frase").html('');
+								$("#letras-palabra").html('');
+								principal(numAleatorio2);
+							}
 							errores=2;
 							$("#intentos").html("<p>Intentos: "+errores+"</p>");
 
-						}else{
+							}else{
 							//Si es incorrecta, lo siguiente:
 							if(errores >1 ){ //Verifiamos si aun le quedan intentos
 								$("#mensajesSistema").html("<p>Vuelve a intentar</p>");
@@ -59,8 +77,11 @@ $.ajax({ // Con la funcion ajax cargamos el XML
 								$("#intentos").html("<p>Intentos: "+errores+"</p>");
 							}
 							
-						}								
-				});				
+							}
+
+										
+				});
+
 			}
 		});
 	},
@@ -68,3 +89,4 @@ $.ajax({ // Con la funcion ajax cargamos el XML
 		$("#letras-palabra").text("Error al obtener xml");
 	}
 });
+}
